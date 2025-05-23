ca961dd586543065054fa44fa46d5c96114e41f0
Sophia <scoreap@correo.url.edu.gt>
2025-05-23 09:00:30 -0600

    .

--- src/main/java/ed/lab/E01MeetingRooms.java
+++ src/main/java/ed/lab/E01MeetingRooms.java
@@ -1,35 +1,20 @@
 package ed.lab;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.PriorityQueue;
-import java.util.Queue;
 
 public class E01MeetingRooms {
-
+    PriorityQueue<Integer> meetingRooms = new PriorityQueue<>((a,b)->a-b);
     public int minMeetingRooms(List<MeetingInterval> meetingIntervals) {
-        //Guardar los valores de intervalos en una cola de prioridades para ordenarlos de menor a mayor
-        Queue<Integer> meetingRooms = new PriorityQueue<>((a,b) -> a - b);
-        Queue<Integer> OccupiedRooms = new PriorityQueue<>((a,b) -> a - b);
-        int traslapes = 0;
-
-        for(MeetingInterval meetingInterval : meetingIntervals){
-            int first = meetingInterval.startTime();
-            int last = meetingInterval.endTime();
-            meetingRooms.offer(first);
-            meetingRooms.offer(last);
-
-            if(meetingRooms.poll() == first) {
-                OccupiedRooms.add(first);
-                traslapes++;
+        meetingIntervals.sort((a, b) -> a.startTime() - b.startTime());
+        for (MeetingInterval interval : meetingIntervals) {
+            if (!meetingRooms.isEmpty() && meetingRooms.peek() <= interval.startTime()) {
+                meetingRooms.poll();
             }
-            else if(meetingRooms.poll() == last) {
-                OccupiedRooms.add(last);
-                traslapes++;
-            }
+            meetingRooms.offer(interval.endTime());
         }
-
-        return traslapes;
+        int result = meetingRooms.size();
+        return result;
     }
 
 }
