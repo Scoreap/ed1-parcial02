75a66e88e783a8070b8ebf792e78510b7827d851
Sophia <scoreap@correo.url.edu.gt>
2025-05-23 08:50:44 -0600

    .

--- src/main/java/ed/lab/E01MeetingRooms.java
+++ src/main/java/ed/lab/E01MeetingRooms.java
@@ -1,11 +1,35 @@
 package ed.lab;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class E01MeetingRooms {
 
     public int minMeetingRooms(List<MeetingInterval> meetingIntervals) {
-        return -1;
+        //Guardar los valores de intervalos en una cola de prioridades para ordenarlos de menor a mayor
+        Queue<Integer> meetingRooms = new PriorityQueue<>((a,b) -> a - b);
+        Queue<Integer> OccupiedRooms = new PriorityQueue<>((a,b) -> a - b);
+        int traslapes = 0;
+
+        for(MeetingInterval meetingInterval : meetingIntervals){
+            int first = meetingInterval.startTime();
+            int last = meetingInterval.endTime();
+            meetingRooms.offer(first);
+            meetingRooms.offer(last);
+
+            if(meetingRooms.poll() == first) {
+                OccupiedRooms.add(first);
+                traslapes++;
+            }
+            else if(meetingRooms.poll() == last) {
+                OccupiedRooms.add(last);
+                traslapes++;
+            }
+        }
+
+        return traslapes;
     }
 
 }
--- src/main/java/ed/lab/E02Logger.java
+++ src/main/java/ed/lab/E02Logger.java
@@ -1,12 +1,27 @@
 package ed.lab;
 
+import java.util.HashMap;
+
 public class E02Logger {
 
     public E02Logger() {
     }
+    HashMap<String, Integer> mapa =  new HashMap<>();
 
     public boolean shouldPrintMessage(int timestamp, String message) {
-        return false;
+
+        int prevTimeStamp = timestamp;
+        int nextTimeStamp = 10;
+        mapa.put(message, timestamp);
+        while(mapa.get(message) != null){
+            if(mapa.containsKey(message)){
+                return false;
+                //nextTimeStamp++;
+
+            }
+        }
+
+        return true;
     }
 
 }
