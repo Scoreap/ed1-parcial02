07d8fb9188484a0fc8c080ff40d21e367c903bd3
Sophia <scoreap@correo.url.edu.gt>
2025-05-23 20:43:48 -0600

    final final

--- src/main/java/ed/lab/E02Logger.java
+++ src/main/java/ed/lab/E02Logger.java
@@ -3,25 +3,22 @@ package ed.lab;
 import java.util.HashMap;
 
 public class E02Logger {
-
-    public E02Logger() {
-    }
     HashMap<String, Integer> mapa =  new HashMap<>();
 
+    public E02Logger() {
+        mapa = new HashMap<>();
+    }
+
+
     public boolean shouldPrintMessage(int timestamp, String message) {
+        //segundo intento :(
 
-        int prevTimeStamp = timestamp;
-        int nextTimeStamp = 10;
-        mapa.put(message, timestamp);
-        while(mapa.get(message) != null){
-            if(mapa.containsKey(message)){
-                return false;
-                //nextTimeStamp++;
-
-            }
+        if (!mapa.containsKey(message) || timestamp >= mapa.get(message) + 10) {
+            mapa.put(message, timestamp);
+            return true;
+        } else {
+            return false;
         }
-
-        return true;
     }
 
 }
